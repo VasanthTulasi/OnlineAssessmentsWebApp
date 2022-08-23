ef44eead15361a1763b88fce9bc2d19b8915119d
Vasanth <lalithavasanth10@gmail.com>
2022-08-23 01:00:05 +0100

    send email when professor creates assessment

--- server/routes/AssessmentsRoute.js
+++ server/routes/AssessmentsRoute.js
@@ -1,23 +1,59 @@
 const router = require("express").Router();
 const AssessmentsModel = require("../models/AssessmentsModel");
+const UsersModel = require("../Models/UsersModel");
+const sendMail = require("../services/EmailService");
 
 router.post("/saveNewAssessment", async (req, res) => {
   // console.log(JSON.stringify(req.body));
   const assessment = new AssessmentsModel(req.body);
   assessment
     .save()
-    .then(() => res.json({ message: "Assessment saved successfully!" }))
+    .then(async () => {
+      res.json({ message: "Assessment saved successfully!" });
+      let studentEmails = [];
+      const students = await UsersModel.find(
+        { role: "student" },
+        { email: true, assigned_modules: true, _id: false }
+      );
+      for (let i = 0; i < students.length; i++) {
+        if (students[i].assigned_modules.includes(req.body.module_code))
+          studentEmails.push(students[i].email);
+      }
+
+      const assessmentInfoForEmail = {
+        title: req.body.title,
+        duration: req.body.duration_number + " " + req.body.duration_measure,
+        module_code: req.body.module_code,
+        window_start_time: new Date(req.body.window_start_time)
+          .toString()
+          .slice(0, 21),
+        window_end_time: new Date(req.body.window_end_time)
+          .toString()
+          .slice(0, 21),
+        total_marks: req.body.total_marks,
+      };
+      // console.log("mail data: " + JSON.stringify(assessmentInfoForEmail));
+      for (let i = 0; i < studentEmails.length; i++) {
+        sendMail.assessmentScheduledEmail(
+          studentEmails[i],
+          assessmentInfoForEmail
+        );
+      }
+    })
     .catch((err) => res.json({ message: err }));
 });
-
 
 router.post("/updateAssessmentById", async (req, res) => {
   // console.log(req.body._id);
   // console.log(JSON.stringify(req.body.assessment));
-  const assessments = await AssessmentsModel.replaceOne({
-    _id: req.body._id
-  },{...req.body.assessment}).then(() => res.json({ "message": "Assessment Edited Successfully!" })).catch(err => res.json({"message":err}));
-
+  const assessments = await AssessmentsModel.replaceOne(
+    {
+      _id: req.body._id,
+    },
+    { ...req.body.assessment }
+  )
+    .then(() => res.json({ message: "Assessment Edited Successfully!" }))
+    .catch((err) => res.json({ message: err }));
 });
 
 router.post("/assessmentsForModule", async (req, res) => {
@@ -38,25 +74,21 @@ router.post("/assessmentsForModule", async (req, res) => {
   }
 });
 
-
-router.post("/deleteAssessmentFromModule", (req,res) =>{
-    const {_id} = req.body;
-    AssessmentsModel.deleteOne({_id: _id}, function (err) {
-        if (err) return res.json({ message: err });
-        res.json({ message: "success" });
-      });
+router.post("/deleteAssessmentFromModule", (req, res) => {
+  const { _id } = req.body;
+  AssessmentsModel.deleteOne({ _id: _id }, function (err) {
+    if (err) return res.json({ message: err });
+    res.json({ message: "success" });
+  });
 });
-
 
 router.post("/assessmentsbyId", async (req, res) => {
   const assessments = await AssessmentsModel.find({
-    _id: req.body._id
+    _id: req.body._id,
   });
   if (assessments) {
     res.send(assessments[0]);
   }
 });
-
-
 
 module.exports = router;
--- server/services/EmailService.js
+++ server/services/EmailService.js
@@ -2,71 +2,103 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    auth: {
-      user: "lalithavasanth10@gmail.com",
-      pass: "yizlemewhqwkqhjx"
+  host: "smtp.gmail.com",
+  port: 465,
+  auth: {
+    user: "lalithavasanth10@gmail.com",
+    pass: "yizlemewhqwkqhjx",
+  },
+});
+
+const sendPasswordResetMail = async (recipientID, token) =>
+  await transporter.sendMail(
+    {
+      from: "Admin AssessOnline <noreply@AssessOnline.com>",
+      to: recipientID,
+      subject: "Password Reset Link - AssessOnline",
+      text:
+        "Please click on the below email link or paste is in the browser to reset your password. The link will expire in 10 minutes.\nLink: http://localhost:3000/resetPassword/" +
+        token,
+    },
+    function (err, inf) {
+      if (err) console.log("Error is " + err.message);
+      else console.log("Success " + inf.response);
     }
-  });
+  );
 
-const sendPasswordResetMail = async (recipientID,token) => await transporter.sendMail({
-    from: 'Admin AssessOnline <noreply@AssessOnline.com>',
-    to: recipientID,
-    subject: "Password Reset Link - AssessOnline",
-    text: "Please click on the below email link or paste is in the browser to reset your password. The link will expire in 10 minutes.\nLink: http://localhost:3000/resetPassword/" + token
-  }, function(err, inf){
-    if(err)
-    console.log("Error is "+err.message);
-    else
-    console.log("Success "+inf.response);
-  });
-
-  const sendRegActivationMail = async (recipientID,token) => await transporter.sendMail({
-    from: 'Admin AssessOnline <noreply@AssessOnline.com>',
-    to: recipientID,
-    subject: "Account Approved - AssessOnline",
-    text: "Dear User,\n\nYour AssessOnline account has been successfully approved by the admin.\n\nPlease click on the below email link or paste is in the browser to activate your account. \nLink: http://localhost:3000/accountActivate/" + token
-  }, function(err, inf){
-    if(err)
-    console.log("Error is "+err.message);
-    else{
-    console.log("Success "+inf.response);
-    console.log("http://localhost:3000/accountActivate/" + token);
+const sendRegActivationMail = async (recipientID, token) =>
+  await transporter.sendMail(
+    {
+      from: "Admin AssessOnline <noreply@AssessOnline.com>",
+      to: recipientID,
+      subject: "Account Approved - AssessOnline",
+      text:
+        "Dear User,\n\nYour AssessOnline account has been successfully approved by the admin.\n\nPlease click on the below email link or paste is in the browser to activate your account. \nLink: http://localhost:3000/accountActivate/" +
+        token,
+    },
+    function (err, inf) {
+      if (err) console.log("Error is " + err.message);
+      else {
+        console.log("Success " + inf.response);
+        console.log("http://localhost:3000/accountActivate/" + token);
+      }
     }
-  });
+  );
 
-  const sendRegApprovalMail = async (recipientID) => await transporter.sendMail({
-    from: 'Admin AssessOnline <noreply@AssessOnline.com>',
+const sendRegApprovalMail = async (recipientID) =>
+  await transporter.sendMail(
+    {
+      from: "Admin AssessOnline <noreply@AssessOnline.com>",
+      to: recipientID,
+      subject: "Account Activated - Admin AssessOnline",
+      text: "Dear User,\n\nYour AssessOnline account is activated. You can now login to the application.\n\nPlease reach out to the admin in case of any issues.\n\nRegards,\nAssessOnline.",
+    },
+    function (err, inf) {
+      if (err) console.log("Error is " + err.message);
+      else console.log("Success " + inf.response);
+    }
+  );
+
+const sendRegRejectionMail = async (recipientID, rejectionReason) =>
+  await transporter.sendMail(
+    {
+      from: "Admin AssessOnline <noreply@AssessOnline.com>",
+      to: recipientID,
+      subject: "Registration Denied - Admin AssessOnline",
+      text:
+        "Dear User,\n\nYour request for AssessOnline account has been denied by the admin with the reason mentioned below. Please reach out to the admin for further details.\n\nReason: " +
+        rejectionReason +
+        "\n\nRegards,\nAssessOnline.",
+    },
+    function (err, inf) {
+      if (err) console.log("Error is " + err.message);
+      else console.log("Success " + inf.response);
+    }
+  );
+
+const sendAssessmentScheduledEmail = async (recipientID, assessmentInfo) =>
+  await transporter.sendMail({
+    from: "Admin AssessOnline <noreply@AssessOnline.com>",
     to: recipientID,
-    subject: "Account Activated - Admin AssessOnline",
-    text: "Dear User,\n\nYour AssessOnline account is activated. You can now login to the application.\n\nPlease reach out to the admin in case of any issues.\n\nRegards,\nAssessOnline."
-  }, function(err, inf){
-    if(err)
-    console.log("Error is "+err.message);
-    else
-    console.log("Success "+inf.response);
+    subject: "Assessment Scheduled - Admin AssessOnline",
+    text:
+      "Dear Student,\n\nThis is to notify you that a new assessment has been scheduled for one of your course modules. Please find the details below.\n" +
+      "\nModule Code: " +
+      assessmentInfo.module_code +
+      "\nAssessment Duration: " +
+      assessmentInfo.duration +
+      "\nAssessment Window Start Time: " +
+      assessmentInfo.window_start_time +
+      "\nAssessment Window End Time: " +
+      assessmentInfo.window_end_time +
+      "\nAssessment Total Marks: " +
+      assessmentInfo.total_marks +
+      "\n\nPlease keep checking 'View My Assessments' page in AssessOnline to view any updates for this assessment." +
+      "\n\nRegards,\nAssessOnline.",
   });
-
-  const sendRegRejectionMail = async (recipientID,rejectionReason) => await transporter.sendMail({
-    from: 'Admin AssessOnline <noreply@AssessOnline.com>',
-    to: recipientID,
-    subject: "Registration Denied - Admin AssessOnline",
-    text: "Dear User,\n\nYour request for AssessOnline account has been denied by the admin with the reason mentioned below. Please reach out to the admin for further details.\n\nReason: "+rejectionReason+"\n\nRegards,\nAssessOnline."
-  }, function(err, inf){
-    if(err)
-    console.log("Error is "+err.message);
-    else
-    console.log("Success "+inf.response);
-  });
-
 
 module.exports.passwordResetMail = sendPasswordResetMail;
 module.exports.regApprovalMail = sendRegApprovalMail;
 module.exports.regActivationMail = sendRegActivationMail;
 module.exports.regRejectionMail = sendRegRejectionMail;
-
-
-
-
-
+module.exports.assessmentScheduledEmail = sendAssessmentScheduledEmail;
